7c42657da405507eef853597252f21168116c29b
Vinay <vinayhegde@outlook.com>
2020-09-26 13:12:46 +0530

    Initial changes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <View>
+           <Text style={styles.titleHead}>RummyScore</Text>
     </View>
-  );
-}
+    
+    <View style={styles.view}>
+      <Button
+        title="New Game" 
+        color="#0097a7"
+        onPress={() => Alert.alert('Under construction')}
+      />
+    </View>
+    <Separator />
+    <View style={styles.view}>
+      <Button
+        title="Previous Games"
+        color="#0097a7"
+        onPress={() => Alert.alert('Under construction')}
+      />
+    </View>
+    <Separator />
+    <View style={styles.view}>
+    <Button
+        title="Help"
+        color="#0097a7"
+        onPress={() => Alert.alert('Nothing\'s there to help yet')}
+      />
+    </View>
+    <Separator />
+    <View style={styles.view}>
+    <Button
+        title="About"
+        color="#0097a7"
+        onPress={() => Alert.alert('Alchemy Studios \nApp version: v0.1.0')}
+      />
+    </View>
+    <View>
+      <Text style={{marginTop: 10,textAlign:"right", marginHorizontal: 16}}>v0.0.1</Text>
+    </View>
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    // marginHorizontal: 16,
+    backgroundColor:"#e3f2fd"
   },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginHorizontal: 16,
+  },
+  titleHead: {
+    color:"#0097a7",
+    textAlign:"center", 
+    marginBottom: 100, 
+    fontSize:30, 
+    fontWeight:"bold"
+  },
+  view:{
+    marginHorizontal: 16,
+  }
 });
+
+export default App;
