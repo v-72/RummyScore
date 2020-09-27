ce1759eaa1c213291ab58c1e56323f95a27c5321
Vinay <vinayhegde@outlook.com>
2020-09-27 15:38:38 +0530

    Grid css

--- screens/table.tsx
+++ screens/table.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert, ImageBackground } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, ScrollView } from 'react-native';
 
 export default class Table extends React.Component {
     getCell(player){
-        return (<View key={3} style={{borderWidth:1}}><Text style={{fontWeight: "bold"}}>{player}</Text></View>)
+        return (
+            <View key={3} style={styles.cell}>
+                <Text style={{fontWeight: "bold"}}>{player}</Text>
+            </View>
+            )
     }
 
     renderHeader(i){
-       return( <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }} key={i}>
+       return( <View style={styles.row} key={i}>
                     {this.getCell("Round")}
                     {
                         this.props.players.map((player,i) => { 
@@ -23,10 +27,10 @@ export default class Table extends React.Component {
             return this.renderHeader(i);
         }else{
             return (
-                <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }} key={i}>
-                    <View key={3} style={{borderWidth:1}}><Text>{i}</Text></View>
-                    <View key={3} style={{borderWidth:1}}><Text>3</Text></View>
-                    <View key={4} style={{borderWidth:1}}><Text>4</Text></View>
+                <View style={styles.row} key={i}>
+                    <View key={3} style={styles.cell}><Text style={{fontWeight:"bold",alignSelf:"center"}}>{i}</Text></View>
+                    <View key={3} style={styles.cell}><Text></Text></View>
+                    <View key={4} style={styles.cell}><Text></Text></View>
                 </View>
             );
         }
@@ -35,13 +39,36 @@ export default class Table extends React.Component {
     render() {
         const data = [...Array(parseInt(this.props.rounds)+1).keys()];
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollView}>
+            <View style={styles.main}>
             {
                 data.map((datum,i) => { 
                     return this.renderRow(i,this.props.players[i]);
                 })
             }
             </View>
+            </ScrollView>
+            </SafeAreaView>
         );
     }
   }
+
+
+  const styles = StyleSheet.create({
+    main: { 
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center' 
+    },
+    row:{ 
+        flex: 1, 
+        alignSelf: 'stretch', 
+        flexDirection: 'row' 
+    },
+    cell:{
+        width:100,
+        borderWidth:1,
+        justifyContent: 'center' 
+    }
+  });
